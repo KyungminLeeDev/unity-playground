73e6e6957c42ecdff9437d482fca6ac6b85f3bb4
Jacob <kyungmin.lee.dev@gmail.com>
2024-10-15 18:10:09 +0900

    [#1][B54][C04] 기본 동글 로직

---
+++ Goldmetal/DongleFamily/Assets/Scripts/Dongle.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dongle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 마우스 위치를 월드 좌표로 변환
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // x축 경계 설정
+        float leftBorder = -4.2f + transform.localScale.x / 2f;
+        float rightBorder = 4.2f - transform.localScale.x / 2f;
+        if (mousePos.x < leftBorder)
+        {
+            mousePos.x = leftBorder;
+        }
+        else if (mousePos.x > rightBorder)
+        {
+            mousePos.x = rightBorder;
+        }
+
+        // x, y 축 고정
+        mousePos.y = 8;
+        mousePos.z = 0;
+
+        // 자신의 위치에 변환된 마우스 위치를 적용
+        // - Vector3.Lerp : 목표지점으로 부드럽게 이동
+        transform.position = Vector3.Lerp(transform.position, mousePos, 0.2f);
+    }
+}
