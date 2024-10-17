876bba47789ca1da54f52548637e1080e1a49ca2
Jacob <kyungmin.lee.dev@gmail.com>
2024-10-17 11:23:47 +0900

    [#1][B54][C05] 끌어다 놓기

--- Goldmetal/DongleFamily/Assets/Scripts/Dongle.cs
+++ Goldmetal/DongleFamily/Assets/Scripts/Dongle.cs
@@ -4,36 +4,56 @@ using UnityEngine;
 
 public class Dongle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isDrag;
+    Rigidbody2D rigid;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        // 마우스 위치를 월드 좌표로 변환
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        // x축 경계 설정
-        float leftBorder = -4.2f + transform.localScale.x / 2f;
-        float rightBorder = 4.2f - transform.localScale.x / 2f;
-        if (mousePos.x < leftBorder)
+        if (isDrag)
         {
-            mousePos.x = leftBorder;
-        }
-        else if (mousePos.x > rightBorder)
-        {
-            mousePos.x = rightBorder;
-        }
+            // 마우스 위치를 월드 좌표로 변환
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        // x, y 축 고정
-        mousePos.y = 8;
-        mousePos.z = 0;
+            // x축 경계 설정
+            float leftBorder = -4.2f + transform.localScale.x / 2f;
+            float rightBorder = 4.2f - transform.localScale.x / 2f;
+            if (mousePos.x < leftBorder)
+            {
+                mousePos.x = leftBorder;
+            }
+            else if (mousePos.x > rightBorder)
+            {
+                mousePos.x = rightBorder;
+            }
 
-        // 자신의 위치에 변환된 마우스 위치를 적용
-        // - Vector3.Lerp : 목표지점으로 부드럽게 이동
-        transform.position = Vector3.Lerp(transform.position, mousePos, 0.2f);
+            // x, y 축 고정
+            mousePos.y = 8;
+            mousePos.z = 0;
+
+            // 자신의 위치에 변환된 마우스 위치를 적용
+            // - Vector3.Lerp : 목표지점으로 부드럽게 이동
+            transform.position = Vector3.Lerp(transform.position, mousePos, 0.2f);
+        }
+    }
+
+    public void Drag()
+    {
+        isDrag = true;
+    }
+
+    public void Drop()
+    {
+        isDrag = false;
+        rigid.simulated = true;
     }
 }
