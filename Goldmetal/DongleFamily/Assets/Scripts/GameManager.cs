13b3f0e0a2f814fcff5f9d28618de6816414ddc1
Jacob <kyungmin.lee.dev@gmail.com>
2024-10-18 13:40:49 +0900

    [#1][B55][C02] 동글 생성

--- Goldmetal/DongleFamily/Assets/Scripts/GameManager.cs
+++ Goldmetal/DongleFamily/Assets/Scripts/GameManager.cs
@@ -5,26 +5,42 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Dongle lastDongle;
+    public GameObject donglePrefab;
+    public Transform dongleGroup;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        NextDongle();
     }
 
-    // Update is called once per frame
-    void Update()
+    Dongle GetDongle()
     {
-        
+        GameObject instant = Instantiate(donglePrefab, dongleGroup);
+        Dongle instantDongle = instant.GetComponent<Dongle>();
+        return instantDongle;
+    }
+
+    void NextDongle()
+    {
+        Dongle newDongle = GetDongle();
+        lastDongle = newDongle;
     }
 
     public void TouchDown()
     {
+        if (lastDongle == null)
+            return;
+
         lastDongle.Drag();
     }
 
     public void TouchUp()
     {
+        if (lastDongle == null)
+            return;
+
         lastDongle.Drop();
+        lastDongle = null;
     }
 }
