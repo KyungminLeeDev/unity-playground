a04e0a17025e5649952a57c34c3694698b234659
Jacob <kyungmin.lee.dev@gmail.com>
2024-10-23 18:13:15 +0900

    [#1][BE6][C03] 게임 종료 UI

--- Goldmetal/DongleFamily/Assets/Scripts/GameManager.cs
+++ Goldmetal/DongleFamily/Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.ExceptionServices;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -31,8 +32,10 @@ public class GameManager : MonoBehaviour
     int sfxCursor;
 
     [Header("----------------[ UI ]")]
+    public GameObject endGroup;
     public Text scoreText;
     public Text maxScoreText;
+    public Text subScoreText;
 
 
 
@@ -45,11 +48,13 @@ public class GameManager : MonoBehaviour
 
         donglePool = new List<Dongle>();
         effectPool = new List<ParticleSystem>();
-        for (int index = 0; index < poolSize; index++) {
+        for (int index = 0; index < poolSize; index++)
+        {
             MakeDongle();
-        } 
+        }
 
-        if (!PlayerPrefs.HasKey("MaxScore")) {
+        if (!PlayerPrefs.HasKey("MaxScore"))
+        {
             PlayerPrefs.SetInt("MaxScore", 0);
         }
 
@@ -81,11 +86,13 @@ public class GameManager : MonoBehaviour
         return instantDongle;
     }
 
-    Dongle  GetDongle()
+    Dongle GetDongle()
     {
-        for (int index = 0; index < donglePool.Count; index++) {
+        for (int index = 0; index < donglePool.Count; index++)
+        {
             poolCursior = (poolCursior + 1) % donglePool.Count;
-            if (!donglePool[poolCursior].gameObject.activeSelf) {
+            if (!donglePool[poolCursior].gameObject.activeSelf)
+            {
                 return donglePool[poolCursior];
             }
         }
@@ -95,7 +102,8 @@ public class GameManager : MonoBehaviour
 
     void NextDongle()
     {
-        if (isOver) {
+        if (isOver)
+        {
             return;
         }
 
@@ -109,7 +117,8 @@ public class GameManager : MonoBehaviour
 
     IEnumerator WaitNext()
     {
-        while(lastDongle != null) {
+        while (lastDongle != null)
+        {
             yield return null;
         }
 
@@ -137,11 +146,12 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
-        if (isOver) {
+        if (isOver)
+        {
             return;
         }
         isOver = true;
-        
+
         StartCoroutine(GameOverRoutine());
     }
 
@@ -151,27 +161,49 @@ public class GameManager : MonoBehaviour
         Dongle[] dongles = GameObject.FindObjectsOfType<Dongle>();
 
         // 2. 지우기 전에 모든 동글의 물리효과 비활성화
-        for (int index = 0; index < dongles.Length; index++) {
+        for (int index = 0; index < dongles.Length; index++)
+        {
             dongles[index].rigid.simulated = false;
         }
 
         // 3. 1번의 목록을 하나씩 접근해서 지우기 -> 점수 정리
-        for (int index = 0; index < dongles.Length; index++) {
+        for (int index = 0; index < dongles.Length; index++)
+        {
             dongles[index].Hide(Vector3.up * 100); // 게임플레이중 나올수 없는 큰값을 넣어서 구분시켜서 임시로 해결
             yield return new WaitForSeconds(0.1f);
         }
 
         yield return new WaitForSeconds(1.0f);
 
+        // 최고 점수 갱신
         int maxScore = Mathf.Max(score, PlayerPrefs.GetInt("MaxScore"));
         PlayerPrefs.SetInt("MaxScore", maxScore);
 
+        // 게임오버 UI 표시
+        subScoreText.text = "점수 : " + scoreText.text;
+        endGroup.SetActive(true);
+
+        bgmPlayer.Stop();
         SfxPlay(Sfx.Over);
+    }
+
+    public void Reset()
+    {
+        SfxPlay(Sfx.Button);
+        StartCoroutine(ResetCoroutine());
+    }
+
+    IEnumerator ResetCoroutine() 
+    {
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene("Main");
+
     }
 
     public void SfxPlay(Sfx type)
     {
-        switch (type) {
+        switch (type)
+        {
             case Sfx.LevelUp:
                 sfxPlayer[sfxCursor].clip = sfxClip[Random.Range(0, 3)];
                 break;
