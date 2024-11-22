788594f47b5171a2073ffc12cde0fea687865df1
Jacob <kyungmin.lee.dev@gmail.com>
2024-11-22 11:12:30 +0900

    [#6][C03] PickRandomCards 완성

--- C#/Head First C# (4th Edition)/Chapter_03/PickRandomCards/PickRandomCards/Program.cs
+++ C#/Head First C# (4th Edition)/Chapter_03/PickRandomCards/PickRandomCards/Program.cs
@@ -6,7 +6,26 @@ namespace PickRandomCards
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the number of cards to pick: ");
+
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                // line을 int로 변환할 수 있으면 반환된 int 값은
+                // numberOfCards 변수에 저장되고 이 코드 블록 실행
+
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                // line을 int로 변환할 수 없으면 이 코드 블록 실행
+
+                Console.WriteLine("Please enter a valid number.");
+            }
         }
     }
 }
