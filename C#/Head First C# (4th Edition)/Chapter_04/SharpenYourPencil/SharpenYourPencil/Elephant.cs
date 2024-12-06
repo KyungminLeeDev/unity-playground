aecd4e6e91888f56efaa16bb800004252f47135f
Jacob <kyungmin.lee.dev@gmail.com>
2024-12-06 13:38:38 +0900

    SharpenYourPencil

---
+++ C#/Head First C# (4th Edition)/Chapter_04/SharpenYourPencil/SharpenYourPencil/Elephant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpenYourPencil
+{
+    internal class Elephant
+    {
+        public int EarSize;
+        public string Name;
+        public void WhoAmI()
+        {
+            Console.WriteLine("My name is " + Name + ".");
+            Console.WriteLine("My ears are " + EarSize + " inches tail.");
+        }
+
+        public void HearMessage(string message, Elephant whoSaidIt)
+        {
+            Console.WriteLine(Name + " heard a message");
+            Console.WriteLine(whoSaidIt.Name + " said this: " + message);
+        }
+
+        public void SpeakTo(Elephant whoToTalkTo, string message)
+        {
+            whoToTalkTo.HearMessage(message, this);
+        }
+    }
+}
