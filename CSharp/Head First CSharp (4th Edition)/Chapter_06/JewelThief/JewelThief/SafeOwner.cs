d450478e6c7ea808ae5b9310b4242398fe5b561f
Jacob <kyungmin.lee.dev@gmail.com>
2024-12-13 14:11:40 +0900

    [#6][C06] JewelThief

---
+++ CSharp/Head First CSharp (4th Edition)/Chapter_06/JewelThief/JewelThief/SafeOwner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JewelThief
+{
+    internal class SafeOwner
+    {
+        private string valuables = "";
+        public void ReceiveContents(string safeContetns)
+        {
+            valuables = safeContetns;
+            Console.WriteLine($"Thank you for returning my {valuables}!");
+        }
+    }
+}
