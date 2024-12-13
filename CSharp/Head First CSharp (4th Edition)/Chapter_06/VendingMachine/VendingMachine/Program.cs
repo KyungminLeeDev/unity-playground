203fae2536103ed933139d210b39c93dc1dbf5cc
Jacob <kyungmin.lee.dev@gmail.com>
2024-12-13 11:16:01 +0900

    [#6][C06] VendingMachine

---
+++ CSharp/Head First CSharp (4th Edition)/Chapter_06/VendingMachine/VendingMachine/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VendingMachine vendingMachine = new AnimalFeedVendingMachine();
+            Console.WriteLine(vendingMachine.Dispense(2.00M));
+
+
+        }
+    }
+}
