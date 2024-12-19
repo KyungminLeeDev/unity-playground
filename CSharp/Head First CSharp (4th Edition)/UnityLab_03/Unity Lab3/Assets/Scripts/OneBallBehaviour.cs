0041b3d1de19526f098b5212555a7170af603935
Jacob <kyungmin.lee.dev@gmail.com>
2024-12-19 09:42:42 +0900

    [#6] UnityLab_03 시작

---
+++ CSharp/Head First CSharp (4th Edition)/UnityLab_03/Unity Lab3/Assets/Scripts/OneBallBehaviour.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneBallBehaviour : MonoBehaviour
+{
+    public float XRotation = 0;
+    public float YRotation = 1;
+    public float ZRotation = 0;
+    public float DegreesPerSecond = 180;
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
