f19c284ffcc9605780322c5e26b8abb6a2d70447
Jacob <kyungmin.lee.dev@gmail.com>
2024-12-19 10:44:57 +0900

    [#6] UnityLab_03 완료

--- CSharp/Head First CSharp (4th Edition)/UnityLab_03/Unity Lab3/Assets/Scripts/OneBallBehaviour.cs
+++ CSharp/Head First CSharp (4th Edition)/UnityLab_03/Unity Lab3/Assets/Scripts/OneBallBehaviour.cs
@@ -8,19 +8,22 @@ public class OneBallBehaviour : MonoBehaviour
     public float YRotation = 1;
     public float ZRotation = 0;
     public float DegreesPerSecond = 180;
-    
 
-
+    static int BallCount = 0;
+    public int BallNumber;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
+        BallCount++;
+        BallNumber = BallCount;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 axis = new Vector3 (XRotation, YRotation, ZRotation);
+        transform.RotateAround(Vector3.zero, axis, DegreesPerSecond * Time.deltaTime);
     }
 }
