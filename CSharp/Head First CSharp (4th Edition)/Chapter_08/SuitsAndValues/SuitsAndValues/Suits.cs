a42da1e3c0f112bde86432b7008a8b7c97a8534f
Jacob <kyungmin.lee.dev@gmail.com>
2024-12-20 17:34:35 +0900

    [#6][C08] SuitsAndValues

---
+++ CSharp/Head First CSharp (4th Edition)/Chapter_08/SuitsAndValues/SuitsAndValues/Suits.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JumbledCards
+{
+    enum Suits
+    {
+        Diamonds,
+        Clubs,
+        Hearts,
+        Spades
+    }
+}
