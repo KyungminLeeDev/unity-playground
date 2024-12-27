b40af82ac23cc18271e35d41b99fb2daea11abd8
Jacob <kyungmin.lee.dev@gmail.com>
2024-12-27 18:09:29 +0900

    [#6][C09] JimmyLinqRefactored

---
+++ CSharp/Head First CSharp (4th Edition)/Chapter_09/JimmyLinqRefactored/JimmyLinqRefactored/Critics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JimmyLinqRefactored
+{
+    enum Critics
+    {
+        MuddyCritic,
+        RottenTornadoes,
+    }
+}
