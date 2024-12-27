098d86c38207198b8e6c8150ccaec461df3b289b
Jacob <kyungmin.lee.dev@gmail.com>
2024-12-27 13:53:05 +0900

    [#6][C09] CreateAnonymousTypes

---
+++ CSharp/Head First CSharp (4th Edition)/Chapter_09/CreateAnonymousTypes/CreateAnonymousTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CreateAnonymousTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 익명 타입
+            // - var 사용하여 익명타입 선언
+            // - 익명타입은 이름이 없는것만 제외하면 다른 타입과 동일
+            var whatAmI = new { Color = "Blue", Flavor = "Tasty", Height = 37 };
+            Console.WriteLine(whatAmI);
+
+            Console.WriteLine($"My color is {whatAmI.Color} and I'm {whatAmI.Flavor}");
+        }
+    }
+}
