cc1cbeb2e5cb64f5385f4c8c8bf9632979d83b2c
Jacob <kyungmin.lee.dev@gmail.com>
2025-01-15 11:18:25 +0900

    [#6][C09] CreateSequenceByHand

---
+++ CSharp/Head First CSharp (4th Edition)/Chapter_09/CreateSequenceByHand/CreateSequenceByHand/Sport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreateSequenceByHand
+{
+    enum Sport
+    {
+        Football,
+        Baseball,
+        Basketball,
+        Hockey,
+        Boxing,
+        Rugby,
+        Fencing
+    }
+}
