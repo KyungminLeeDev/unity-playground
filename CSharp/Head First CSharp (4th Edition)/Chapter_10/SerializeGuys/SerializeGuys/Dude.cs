71271c9ae8021d8dc0544abe07e55d58f5040966
Jacob <kyungmin.lee.dev@gmail.com>
2025-01-17 17:55:02 +0900

    [#6][C10] SerializeGuys

---
+++ CSharp/Head First CSharp (4th Edition)/Chapter_10/SerializeGuys/SerializeGuys/Dude.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerializeGuys
+{
+    internal class Dude
+    {
+        public string Name { get; set; }
+        public HairStyle Hair { get; set; }
+    }
+}
