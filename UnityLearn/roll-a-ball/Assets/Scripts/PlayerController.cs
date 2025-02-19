9abf5f8d3126742cd249c70a2743f2f4150b8570
Jacob <kyungmin.lee.dev@gmail.com>
2025-02-19 17:20:21 +0900

    [#10] 2.5 Write the OnMove function declaration

--- UnityLearn/roll-a-ball/Assets/Scripts/PlayerController.cs
+++ UnityLearn/roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,9 +12,9 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue movementValue)
     {
-        
+
     }
+    
 }
