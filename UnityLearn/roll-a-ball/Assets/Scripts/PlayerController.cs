d416cca01577954636c377d30a62c9bb4c3480f3
Jacob <kyungmin.lee.dev@gmail.com>
2025-02-20 13:59:28 +0900

    [#10] 2.6 Apply input data to the Player

--- UnityLearn/roll-a-ball/Assets/Scripts/PlayerController.cs
+++ UnityLearn/roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+
+
     // Start is called before the first frame update
     void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
     {
         
     }
 
     void OnMove(InputValue movementValue)
     {
-
+        Vector2 movementVector = movementValue.Get<Vector2>();
     }
+
     
+
+    
+
 }
